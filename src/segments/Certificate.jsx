a76b91fca68a1bcaeca4d8deebec2e0492e14d3e
arzerra <ianclark1506@gmail.com>
2025-10-04 02:11:34 +0800

    added certificate of completion from infinit solutions

--- src/segments/Certificate.jsx
+++ src/segments/Certificate.jsx
@@ -8,27 +8,26 @@ function Certificate() {
     {
       img: "images/certificates/1.png",
       title: "Cybersecurity",
-      desc: "An LMS intended to minimize cheating behaviors for online assessments and provides a mechanism to detect and store cheating behavior with face and object detection.",
     },
     {
       img: "images/certificates/2.png",
       title: "HTML and CSS",
-      desc: "A functional website with a sole purpose to provide a convenient way for visitors in Davao City to browse, compare, and rent cars for personal or business use.",
     },
     {
       img: "images/certificates/3.png",
       title: "Network Security",
-      desc: "Developed the school’s official website with responsive UI, intuitive navigation, and content management for announcements, programs, and enrollment.",
     },
     {
       img: "images/certificates/4.png",
       title: "JavaScript",
-      desc: "A fullstack MERN app with authentication and dashboards.",
     },
     {
       img: "images/certificates/5.png",
       title: "ReactJS",
-      desc: "A fullstack MERN app with authentication and dashboards.",
+    },
+    {
+      img: "images/certificates/6.jpg",
+      title: "Certificate of Completion",
     },
   ];
 
@@ -49,7 +48,7 @@ function Certificate() {
             <div
               key={i}
               className="snap-center flex-none w-80 flex-col rounded-2xl bg-gradient-to-br from-white to-gray-50 text-gray-700 font-['Quicksand'] shadow-lg hover:shadow-xl transition-all duration-300 hover:-translate-y-3 cursor-pointer"
-              onClick={() => setSelectedImg(cert.img)} // set clicked image
+              onClick={() => setSelectedImg(cert.img)}
             >
               <div className="relative mx-4 -mt-10 h-50 overflow-hidden rounded-2xl shadow-lg group">
                 <img
